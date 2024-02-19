da906e7c5b9e4fc653db6853647653f7be3a0aff
youssef <youssefelghamour@gmail.com>
2024-02-19 01:23:29 -0800

    DBStorage - States and Cities: City

--- models/city.py
+++ models/city.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """ City module """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ city class that inherits from BaseModel """
 
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
