119b31aa317b17f18e5e10bd03dc751b986640f4
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-02-19 13:35:34 -0800

    7. DBStorage - User

--- models/user.py
+++ models/user.py
@@ -2,20 +2,13 @@
 """ User class """
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
-from os import getenv
 
 
 class User(BaseModel, Base):
     """ class User that inherits from BaseModel """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
+    __tablename__ = 'users'
 
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-    else:
-         email = ""
-         password = ""
-         first_name = ""
-         last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
