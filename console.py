63eb8817ccfa8813468c0e1641d8af86d4ac7c48
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-02-19 05:23:43 -0800

    2. Console improvements

--- console.py
+++ console.py
@@ -25,7 +25,7 @@ class HBNBCommand(cmd.Cmd):
         '''Creates a new instance of BaseModel, saves it (to the JSON file)'''
         line = line.replace('"','')
         args = line.split()
-        if line == '':
+        if args[0] == '':
             print("** class name missing **")
         elif args[0] not in storage.classes:
             print("** class doesn't exist **")
