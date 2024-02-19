e6f493b507605da68e65c59c30c4aed57fb9ec2b
youssef <youssefelghamour@gmail.com>
2024-02-19 01:53:38 -0800

    6. DBStorage - States and Cities: State

--- models/state.py
+++ models/state.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 """ State module """
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
+from models.city import City
+import models
 
 
 class State(BaseModel):
     """ state class that inherits from BaseModel """
+    __tablename__ = "states"
 
-    name = ""
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete-orphan",
+                          backref="state")
+
+    @property
+    def cities(self):
+        """Getter attribute that returns the list of City instances"""
+        city_list = []
+        for city in models.storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
