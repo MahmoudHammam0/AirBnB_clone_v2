8c01744320bfcb561636bde217bb9e09b72be3ac
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-02-19 14:36:25 -0800

    8. DBStorage - Place

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -50,7 +50,7 @@ class DBStorage:
         objects_dict = {}
 
         if cls is None:
-            classes_to_query = [User, State, City]
+            classes_to_query = [User, State, City, Place]
         else:
             classes_to_query = [cls]
 
