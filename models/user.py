ed556ab4f6331ed9bd1f75af7e1fea6a639a04d6
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-02-19 12:28:14 -0800

    7. DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ User class """
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, event
 from models.base_model import BaseModel, Base
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -9,6 +10,16 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
+    _password = Column('password', String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+
+    @property
+    def password(self):
+        """Getter method for password"""
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        """Setter method for password that automatically hashes the value"""
+        self._password = hashlib.sha256(value.encode()).hexdigest()
