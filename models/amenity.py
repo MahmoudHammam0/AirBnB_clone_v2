28a4a27571aa49caeb41b46c30ad5971cc813368
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-02-20 10:28:46 -0800

    10. DBStorage - Amenity... and BOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -10,5 +10,4 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
 
     name = Column(String(128), nullable=False)
-    place_amenitites = relationship("Place", secondary="place_amenity",
-                                    back_populates="amenities")
+    place_amenitites = relationship("Place", secondary="place_amenity")
