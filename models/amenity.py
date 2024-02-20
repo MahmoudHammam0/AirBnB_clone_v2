6aaeb7f6471085556ac27696035a0e5899c5ad4d
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-02-20 13:26:04 -0800

    10. DBStorage - Amenity... and BOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -11,4 +11,4 @@ class Amenity(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     place_amenitites = relationship("Place", secondary='place_amenity',
-                                    back_populates='amenities')
+                                    backref='amenities')
--- models/place.py
+++ models/place.py
@@ -34,7 +34,7 @@ class Place(BaseModel, Base):
     reviews = relationship("Review", cascade="all, delete-orphan",
                            backref='place')
     amenities = relationship("Amenity", secondary=place_amenity,
-                             viewonly=False, back_populates='place_amenities')
+                             viewonly=False, backref='place_amenities')
 
     @property
     def reviews(self):
