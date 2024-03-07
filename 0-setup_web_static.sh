ef1e7d15262a63e946eadec4cc784eebf779baca
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-06 22:09:26 -0800

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,24 +4,14 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo chown -HR ubuntu:ubuntu /data/
 echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-echo "
-server {
-	listen 80;
-
-	location /hbnb_static {
-		alias /data/web_static/current/;
-		index index.html;
-	}
-}" | sudo tee /etc/nginx/sites-available/hbnb_static > /dev/null
-sudo rm -f /etc/nginx/sites-enabled/default
-sudo ln -sf /etc/nginx/sites-available/hbnb_static /etc/nginx/sites-enabled/
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -HR ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
