8b9642b7450d1f81d06e51e626223b588e49d9c7
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-07 03:00:05 -0800

    3. Full deployment

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Full deployment module'''
+import os
+from fabric.api import env
+do_pack = __import__('1-pack_web_static').do_pack
+do_dep = __import__('2-do_deploy_web_static').do_deploy
+
+
+env.hosts = ["ubuntu@54.197.110.80", "ubuntu@100.24.236.222"]
+
+
+def deploy():
+    res = do_pack()
+    if os.path.exists(res):
+        pass
+    else:
+        return False
+    end = do_dep(res)
+    return end
