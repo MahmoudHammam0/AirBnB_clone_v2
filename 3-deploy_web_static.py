e49feeb97a9d4d3fa8288f4715e19414096e30c9
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-07 03:36:44 -0800

    3. Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,20 +1,52 @@
 #!/usr/bin/python3
 '''Full deployment module'''
 import os
-from fabric.api import env
-do_pack = __import__('1-pack_web_static').do_pack
-do_dep = __import__('2-do_deploy_web_static').do_deploy
+from fabric.api import put, run, env, local
+from datetime import datetime
 
 
 env.hosts = ["ubuntu@54.197.110.80", "ubuntu@100.24.236.222"]
 
 
-def deploy():
-    '''calls the two functions of do_pack and do_deploy'''
-    res = do_pack()
-    if os.path.exists(res):
+def do_pack():
+    '''packs the content of web_static into a tgz archive'''
+    time = datetime.now()
+    time_str = time.strftime("%Y%m%d%H%M%S")
+    try:
+        filename = "versions/web_static_" + time_str + ".tgz"
+        local('mkdir -p versions/')
+        local('tar -cvzf {} web_static'.format(filename))
+        return filename
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    '''distributes an archive to your web servers'''
+    if os.path.exists(archive_path):
         pass
     else:
         return False
-    end = do_dep(res)
-    return end
+    tgz_file = archive_path.split('/')[1]
+    filename = tgz_file.split('.')[0]
+    loc = '/data/web_static/releases/'
+    try:
+        put(archive_path, "/tmp/")
+        run('mkdir -p {}{}/'.format(loc, filename))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(tgz_file, loc, filename))
+        run('rm /tmp/{}'.format(tgz_file))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(loc, filename))
+        run('rm -rf {}{}/web_static'.format(loc, filename))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(loc, filename))
+        return True
+    except Exception:
+        return False
+
+
+def deploy():
+    '''calls the two functions of do_pack and do_deploy'''
+    res = do_pack()
+    if res is None:
+        return False
+    return do_deploy(res)
