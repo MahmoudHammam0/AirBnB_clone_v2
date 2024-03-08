e93e65668cd85a85562dc5fb926e0a0901563a80
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-07 17:04:56 -0800

    4. Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,11 +8,8 @@ env.hosts = ["ubuntu@54.197.110.80", "ubuntu@100.24.236.222"]
 
 def do_clean(number=0):
     '''deletes out-of-date archives'''
-    if int(number) == 0:
-        number = 1
-    else:
-        number = int(number)
+    number = 1 if int(number) == 0 else int(number)
     number += 1
-    local('cd versions; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    loc = '/data/web_static/releases'
-    run('cd {}; ls -t | tail -n +{} | xargs rm -rf'.format(loc, number))
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
