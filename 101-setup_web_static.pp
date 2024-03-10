12a79c513aecc7d37c1e9b890893d6c3ced6aca6
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-10 10:09:19 -0700

    5. Puppet for setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,47 +1,64 @@
-#sets up your web servers for the deployment
--> exec {'sudo apt-get -y update':
-  provider => shell,
-}
+# Puppet for setup
 
--> exec {'sudo apt-get -y install nginx':
-  provider => shell,
+$config_file = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://www.google.com;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
 }
 
 -> file { '/data':
-  ensure => 'directory',
+  ensure  => 'directory'
 }
 
 -> file { '/data/web_static':
-  ensure => 'directory',
+  ensure => 'directory'
 }
 
 -> file { '/data/web_static/releases':
-  ensure => 'directory',
+  ensure => 'directory'
 }
 
 -> file { '/data/web_static/releases/test':
-  ensure => 'directory',
+  ensure => 'directory'
 }
 
 -> file { '/data/web_static/shared':
-  ensure => 'directory',
+  ensure => 'directory'
 }
 
 -> file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "Holberton School\n",
+  content => "Holberton School\n"
 }
 
 -> file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test',
+  target => '/data/web_static/releases/test'
 }
 
 -> exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/',
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
--> file { '/var/www':
+file { '/var/www':
   ensure => 'directory'
 }
 
@@ -51,20 +68,18 @@
 
 -> file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "Hello World!"
 }
 
 -> file { '/var/www/html/404.html':
   ensure  => 'present',
-  content => "Ceci n'est pas une page"
+  content => "Ceci n'est pas une page\n"
 }
 
--> exec {'/etc/nginx/sites-available/default':
-  ensure => 'present',
-  command  => "sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;} /etc/nginx/sites-enabled/default",
+-> file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $config_file
 }
 
--> exec {'restart Nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
+-> exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
