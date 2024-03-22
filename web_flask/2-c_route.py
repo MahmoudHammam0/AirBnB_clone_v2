7e61095e721ebc1a26654ea3258d7beba706f5c8
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-22 05:13:48 -0700

    2. C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'HBNB module'
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    'Hello HBNB!'
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    'HBNB home'
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    'C + text'
+    new = ""
+    for char in text:
+        if char == '_':
+            new += " "
+            continue
+        new += char
+    return f'C {new}'
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
