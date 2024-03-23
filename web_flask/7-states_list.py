ba1283a29000d8aff8a48a002962a84c5647e6b7
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-23 13:05:06 -0700

    8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 'List of states module'
-from flask import Flask, render_template, g
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
@@ -15,18 +15,10 @@ def list_states():
     return render_template('7-states_list.html', states_list=states_list)
 
 
-def get_db():
-    if 'storage' not in g:
-        g.storage = storage
-    return g.storage
-
-
 @app.teardown_appcontext
-def session_close(exception):
+def app_teardown(exception):
     'close sql alchemy session after each request'
-    storage = g.pop('storage', None)
-    if storage is not None:
-        storage.close()
+    storage.close()
 
 
 if __name__ == "__main__":
