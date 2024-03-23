404e1e11e24bba223b42d2555dcd135c0199bc43
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-23 04:37:52 -0700

    5. Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def num(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def temp_num(n):
     'display template only if n was int'
-    return render_template('5-number.html')
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
