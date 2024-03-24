4a2def26322fda92795f13f39ef6bbf9d4cd1cae
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-23 21:41:49 -0700

    11. HBNB filters

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.amenity import Amenity
 
 
 app = Flask(__name__)
@@ -12,7 +13,8 @@ app = Flask(__name__)
 def list_states(id=None):
     'display list cities by states'
     states = storage.all(State).values()
-    return render_template('10-hbnb_filters.html', states=states)
+    amenities = storage.all(Amenity).values()
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
